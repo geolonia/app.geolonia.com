072f7d8360c4e3b062426a7560bd08d6f5d9abdd
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-04-19 12:55:49 +0900

    Add prop-types

--- src/components/origin-list.js
+++ src/components/origin-list.js
@@ -1,8 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const isOrigin = value => /^https?:\/\/.+$/gm.test(value);
 
 export class OriginList extends React.PureComponent {
+  /**
+   * propTypes
+   * @type {object}
+   */
+  static propTypes = {
+    origins: PropTypes.arrayOf(PropTypes.string).isRequired,
+    remove: PropTypes.func.isRequired,
+    add: PropTypes.func.isRequired
+  };
+
   state = { newOrigin: "" };
   render() {
     const { newOrigin } = this.state;
