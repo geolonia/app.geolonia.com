7abe6cff2b0283fce21f620a58581a9745a05312
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-04-19 12:42:44 +0900

    Split app router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import HomeRoute from "./routes/home";
-import SignUpRoute from "./routes/sign-up";
-import SignInRoute from "./routes/sign-in";
-import DashboardRoute from "./routes/dashboard";
+import AppRouter from "./app-router";
 
-const AppRouter = () => (
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sign-in/">{"サインアップ"}</Link>
-          </li>
-          <li>
-            <Link to="/sign-up/">{"サインイン"}</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/">{"ダッシュボード"}</Link>
-          </li>
-        </ul>
-      </nav>
+const App = () => <AppRouter />;
 
-      <Route path="/" exact component={HomeRoute} />
-      <Route path="/sign-in/" component={SignInRoute} />
-      <Route path="/sign-up/" component={SignUpRoute} />
-      <Route path="/dashboard/" component={DashboardRoute} />
-    </div>
-  </Router>
-);
-
-export default AppRouter;
+export default App;
---
+++ src/app-router.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomeRoute from "./routes/home";
+import SignUpRoute from "./routes/sign-up";
+import SignInRoute from "./routes/sign-in";
+import DashboardRoute from "./routes/dashboard";
+
+const AppRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-in/">{"サインアップ"}</Link>
+          </li>
+          <li>
+            <Link to="/sign-up/">{"サインイン"}</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/">{"ダッシュボード"}</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={HomeRoute} />
+      <Route path="/sign-in/" component={SignInRoute} />
+      <Route path="/sign-up/" component={SignUpRoute} />
+      <Route path="/dashboard/" component={DashboardRoute} />
+    </div>
+  </Router>
+);
+
+export default AppRouter;
