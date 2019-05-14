48a708f6e4e4ffd6edac760d020ae7743c46be5b
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-05-14 13:49:52 +0900

    Fix contents and fit to tilecloud

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import AuthContainer from './container/auth';
-import AppRouter from './app-router';
-import 'uikit/dist/css/uikit.min.css';
-import 'isomorphic-fetch';
-import './App.css';
+import React from "react";
+import AuthContainer from "./container/auth";
+import AppRouter from "./app-router";
+import "uikit/dist/css/uikit.min.css";
+import "isomorphic-fetch";
 
 const App = () => <AuthContainer Root={AppRouter} />;
 
