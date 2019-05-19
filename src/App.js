3ac667a68f55b2dbe017ed401fda226be61fed61
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-05-19 17:47:03 +0900

    Use icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import AuthContainer from "./container/auth";
 import AppRouter from "./app-router";
-import "uikit/dist/css/uikit.min.css";
-import "isomorphic-fetch";
 
 const App = () => <AuthContainer Root={AppRouter} />;
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import './config/amplify';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./config/amplify";
+import "isomorphic-fetch";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// CSS
+import "./index.css";
+import "uikit/dist/css/uikit.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
