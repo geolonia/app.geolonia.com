4c689933faeb1fb012a1f4b2b96139d78e2c2cee
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-07-09 16:15:28 +0900

    Fix dummy graph

--- src/components/dummy-chart.js
+++ src/components/dummy-chart.js
@@ -2,48 +2,13 @@ import React from "react";
 import { LineChart, CartesianGrid, Line, ResponsiveContainer } from "recharts";
 
 const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
+  { uv: 4000, pv: 2400 },
+  { uv: 3000, pv: 1398 },
+  { uv: 2000, pv: 3908 },
+  { uv: 2780, pv: 9800 },
+  { uv: 1890, pv: 4800 },
+  { uv: 2390, pv: 3800 },
+  { uv: 3490, pv: 4300 }
 ];
 
 export const DummyChart = () => {
