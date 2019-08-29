cc5c7ae5a795649fe33c8953bd4bd2629ab1f094
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-08-29 09:07:12 +0900

    improve list tables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Paperbase from './components/Paperbase'
 
 const App: React.FC = () => {
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,13 +5,7 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { withStyles,Theme } from '@material-ui/core/styles';
 
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import Table from './Table';
 
 import CodeIcon from '@material-ui/icons/Code';
 import SatelliteIcon from '@material-ui/icons/Satellite';
@@ -23,19 +17,24 @@ const styles = (theme:Theme) => ({
 
 });
 
-const rows = [
-  {name: "My Map", updated: "2019-08-28"},
-  {name: "exmaple.com", updated: "2019-08-28"},
-  {name: "exmaple.jp", updated: "2019-08-28"},
+// TODO: Followings are mock data.
+const rowsAPIKeys = [
+  {id:1111, name: "My Map", updated: "2019-08-28"},
+  {id:1112, name: "exmaple.com", updated: "2019-08-28"},
+  {id:1113, name: "exmaple.jp", updated: "2019-08-28"},
 ];
 
-const handleChangePage = () => {
+const rowsStyles = [
+  {id:1111, name: "My Map", updated: "2019-08-28"},
+  {id:1112, name: "exmaple.com", updated: "2019-08-28"},
+  {id:1113, name: "exmaple.jp", updated: "2019-08-28"},
+];
 
-}
-
-const handleChangeRowsPerPage = () => {
-
-}
+const rowsFeatures = [
+  {id:1111, name: "My Map", updated: "2019-08-28", isPublic: true},
+  {id:1112, name: "exmaple.com", updated: "2019-08-28", isPublic: false},
+  {id:1113, name: "exmaple.jp", updated: "2019-08-28", isPublic: true},
+];
 
 type Props= {
   classes: any
@@ -48,120 +47,22 @@ const Dashboard = (props: Props) => {
     <Grid id="dashboard" container className={classes.root} spacing={2}>
       <Grid item sm={12} md={6}>
         <Paper>
-          <Typography component="h2" className="category-title">MAPs</Typography>
-          <Typography component="h3" className="module-title"><CodeIcon /> API Keys</Typography>
-          <Table className="api-keys">
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell align="right">Updated</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map(row => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.updated}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[5]}
-                  colSpan={3}
-                  count={rows.length}
-                  rowsPerPage={5}
-                  page={0}
-                  SelectProps={{
-                    inputProps: { 'aria-label': 'rows per page' },
-                    native: true,
-                  }}
-                  onChangePage={handleChangePage}
-                  onChangeRowsPerPage={handleChangeRowsPerPage}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
-          <Typography component="h3" className="module-title"><SatelliteIcon /> Styles</Typography>
-          <Table className="api-keys">
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell align="right">Updated</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map(row => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.updated}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[5]}
-                  colSpan={3}
-                  count={rows.length}
-                  rowsPerPage={5}
-                  page={0}
-                  SelectProps={{
-                    inputProps: { 'aria-label': 'rows per page' },
-                    native: true,
-                  }}
-                  onChangePage={handleChangePage}
-                  onChangeRowsPerPage={handleChangeRowsPerPage}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
+          <Typography component="h2" className="module-title"><CodeIcon /> API Keys</Typography>
+          <Table rows={rowsAPIKeys} />
         </Paper>
       </Grid>
+
       <Grid item sm={12} md={6}>
         <Paper>
-          <Typography component="h2">Data</Typography>
-          <Typography component="h3"><RoomIcon /> Features</Typography>
-          <Table className="api-keys">
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell align="right">Updated</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map(row => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row">
-                    {row.name} <span className="private-or-public">Private</span>
-                  </TableCell>
-                  <TableCell align="right">{row.updated}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[5]}
-                  colSpan={3}
-                  count={rows.length}
-                  rowsPerPage={5}
-                  page={0}
-                  SelectProps={{
-                    inputProps: { 'aria-label': 'rows per page' },
-                    native: true,
-                  }}
-                  onChangePage={handleChangePage}
-                  onChangeRowsPerPage={handleChangeRowsPerPage}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
+        <Typography component="h2" className="module-title"><SatelliteIcon /> Styles</Typography>
+          <Table rows={rowsStyles} />
+        </Paper>
+      </Grid>
+
+      <Grid item sm={12} md={6}>
+        <Paper>
+          <Typography component="h2" className="module-title"><RoomIcon /> Features</Typography>
+          <Table rows={rowsFeatures} />
         </Paper>
       </Grid>
     </Grid>
