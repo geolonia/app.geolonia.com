fb4d59bbba8d382314ef72b09c38be24927f7278
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-01 22:58:16 +0900

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Footer.scss'
+
+const Footer = () => {
+
+  return (
+    <footer className="app-footer">
+      Powered by Geolonia Inc.
+    </footer>
+  );
+}
+
+export default Footer;
