55b108b341d7b9e0ced268f541890137516949f5
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-11 02:11:23 +0900

    add mapbox-gl

---
+++ src/components/custom/MapEditor.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import mapboxgl from "mapbox-gl"
+import "mapbox-gl/dist/mapbox-gl.css"
+
+const MapEditor = () => {
+
+  return (
+    <div>Hello</div>
+  );
+}
+
+export default MapEditor;
