ab89d24d608a6f97e59e135441cadc97e71d0b1c
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-11 00:19:41 +0900

    some fix

--- src/components/custom/Save.tsx
+++ src/components/custom/Save.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 type Props= {
   label: string,
@@ -9,13 +12,56 @@ type Props= {
 }
 
 const Save = (props: Props) => {
-  const style = {
+  const [open, setOpen] = React.useState(false);
+
+  const style: React.CSSProperties = {
     marginTop: '1em',
     width: '100%',
-  } as React.CSSProperties
+  }
+
+  const handleSave = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    props.handler(event)
+    setOpen(true)
+  }
+
+  function handleClose(event: React.SyntheticEvent | React.MouseEvent, reason?: string) {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  }
 
   return (
-    <Typography style={style} component="p" paragraph={true} align="right"><Button variant="contained" color="primary" onClick={props.handler}>{props.label}</Button></Typography>
+    <div>
+      <Typography style={style} component="p" paragraph={true} align="right">
+        <Button variant="contained" color="primary" onClick={handleSave}>{props.label}</Button>
+      </Typography>
+      <Snackbar
+        className="snackbar-saved"
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">Saved.</span>}
+        action={[
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            onClick={handleClose}
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
+    </div>
   );
 }
 
