1968480258860f90548bef59e9a7e1de4af1b588
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-12 18:02:55 +0900

    display map

--- src/components/Data/GIS.tsx
+++ src/components/Data/GIS.tsx
@@ -20,7 +20,6 @@ import MapEditor from '../custom/MapEditor'
 const Content = () => {
   const mapStyle: React.CSSProperties = {
     width: '100%',
-    height: '400px',
     border: '1px solid #dedede',
     margin: '1em 0',
   }
