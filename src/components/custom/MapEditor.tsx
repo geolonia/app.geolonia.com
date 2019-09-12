51e34188b26f5b55508a6813ba3f0282e5c59c0f
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-12 16:47:54 +0900

    some fix

--- src/components/custom/MapEditor.tsx
+++ src/components/custom/MapEditor.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-import mapboxgl from "mapbox-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
+class App extends Component {
+  state = {
+    viewport: {
+      width: 400,
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  };
 
-const MapEditor = () => {
-
-  return (
-    <div>Hello</div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <ReactMapGL
+          {...this.state.viewport}
+          onViewportChange={(viewport) => this.setState({ viewport })}
+        />
+      </div>
+    );
+  }
 }
 
-export default MapEditor;
+export default App;
