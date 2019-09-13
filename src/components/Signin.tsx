7061219a72cdfea7afbdee97b72139b293a009f5
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-14 03:44:43 +0900

    added sign in page

---
+++ src/components/Signin.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+
+import './Signin.scss';
+import Logo from './custom/logo.svg';
+import Support from './custom/Support';
+
+type Props= {
+
+}
+
+const Content = (props: Props) => {
+  const handleSignin = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+
+  }
+
+  return (
+    <div className="signin">
+      <div className="container">
+        <div className="form">
+          <img src={Logo} alt="" className="logo" />
+          <h1>Sign in to Geolonia</h1>
+          <label>
+            <h2>Username or email address</h2>
+            <input type="text" />
+          </label>
+          <label>
+            <h2>Password</h2>
+            <input type="text" />
+          </label>
+          <p className="forgot-password"><Link href="#/password">Forgot password?</Link></p>
+          <p><Button variant="contained" color="primary" onClick={handleSignin}>Sign in</Button></p>
+        </div>
+
+        <p>New to Geolonia? <Link href="#/signup">Create an account.</Link></p>
+
+        <div className="support-container"><Support /></div>
+      </div>
+    </div>
+  );
+}
+
+Content.defaultProps = {
+
+};
+
+export default Content;
---
+++ src/components/Signup.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Signup.scss';
+
+type Props= {
+
+}
+
+const Content = (props: Props) => {
+  return (
+    <div className="signup">Signup</div>
+  );
+}
+
+Content.defaultProps = {
+
+};
+
+export default Content;
