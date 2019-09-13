9b762702c993a5bf32164ab4df021e0baf7f1321
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-13 20:40:16 +0900

    some fix

--- src/components/custom/MapEditor.tsx
+++ src/components/custom/MapEditor.tsx
@@ -6,6 +6,10 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 // typeguard
 const isMapboxStyle = (obj: any): obj is mapboxgl.Style => !!obj
 
+type Props = {
+
+}
+
 class App extends Component {
   state = {
     viewport: {
@@ -18,10 +22,29 @@ class App extends Component {
     style: false,
   };
 
+  private mapRef: mapboxgl.Map | false
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      viewport: {
+        width: '100%',
+        height: 400,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+      },
+      style: false,
+    };
+    this.mapRef = false
+  }
+
   componentDidMount() {
     fetch('https://api.geolonia.com/dev/styles/geolonia-basic?key=YOUR-API-KEY')
         .then(res => res.json())
         .then(style => this.setState({ style }))
+
+    console.log(this.mapRef)
   }
 
   render() {
