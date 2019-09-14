f428cfb4baf4c6a2690615c6bd327b9c733f869d
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-14 18:45:43 +0900

    some fix

---
+++ src/components/custom/Alert.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Alert.scss'
+
+type Props = {
+  children: string,
+  type: "success" | "danger" | "warning"
+}
+
+const Alert = (props: Props) => {
+
+  return (
+    <div className="alert"><div className={props.type}>{props.children}</div></div>
+  );
+}
+
+Alert.defaultProps = {
+  type: "success",
+};
+
+export default Alert;
