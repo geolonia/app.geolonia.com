32dedf15ca292060ac2b954a3bdc03397d9cf75f
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-09-19 22:37:36 +0900

    Add session ready state to judge whether we can render dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,53 +1,62 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { withStyles,Theme } from '@material-ui/core/styles';
-import CodeIcon from '@material-ui/icons/Code';
-import RoomIcon from '@material-ui/icons/Room';
-import Link from '@material-ui/core/Link';
-import Hidden from '@material-ui/core/Hidden';
+import React from "react";
+import PropTypes from "prop-types";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { withStyles, Theme } from "@material-ui/core/styles";
+import CodeIcon from "@material-ui/icons/Code";
+import RoomIcon from "@material-ui/icons/Room";
+import Link from "@material-ui/core/Link";
+import Hidden from "@material-ui/core/Hidden";
 
-import { Line } from 'react-chartjs-2';
-import moment from 'moment'
+import { Line } from "react-chartjs-2";
+import moment from "moment";
 
-import Table from './custom/Table';
-import iconPlane from './custom/plane.svg';
+import Table from "./custom/Table";
+import iconPlane from "./custom/plane.svg";
 
-import './Dashboard.scss'
+import AmazonCognitoIdentity from "amazon-cognito-identity-js";
 
-const styles = (theme:Theme) => ({
+import "./Dashboard.scss";
 
-});
+const styles = (theme: Theme) => ({});
 
 // TODO: Followings are mock data.
 const rowsAPIKeys = [
-  {id: 1111, name: "My Map", updated: "2019-08-28"},
-  {id: 1112, name: "example.com", updated: "2019-08-28"},
-  {id: 1113, name: "example.jp", updated: "2019-08-28"},
+  { id: 1111, name: "My Map", updated: "2019-08-28" },
+  { id: 1112, name: "example.com", updated: "2019-08-28" },
+  { id: 1113, name: "example.jp", updated: "2019-08-28" }
 ];
 
 const rowsFeatures = [
-  {id: 1111, name: "和歌山県公衆トイレ", updated: "2019-08-28", isPublic: true},
-  {id: 1112, name: "USGS Earthquake", updated: "2019-08-28", isPublic: false},
-  {id: 1113, name: "テスト", updated: "2019-08-28", isPublic: true},
+  {
+    id: 1111,
+    name: "和歌山県公衆トイレ",
+    updated: "2019-08-28",
+    isPublic: true
+  },
+  { id: 1112, name: "USGS Earthquake", updated: "2019-08-28", isPublic: false },
+  { id: 1113, name: "テスト", updated: "2019-08-28", isPublic: true }
 ];
 
-type Props= {
-  classes: any
-}
+type Props = {
+  session?: AmazonCognitoIdentity.CognitoUserSession;
+  classes: any;
+};
 
 const Dashboard = (props: Props) => {
   const { classes } = props;
 
-  const lastDay = moment().add(1, 'months').date(0).date()
-  const labels = []
+  const lastDay = moment()
+    .add(1, "months")
+    .date(0)
+    .date();
+  const labels = [];
   for (let i = 1; i <= lastDay; i++) {
-    if (1 === i || 0 === (i % 5) || i === lastDay) {
-      labels.push(i)
+    if (1 === i || 0 === i % 5 || i === lastDay) {
+      labels.push(i);
     } else {
-      labels.push('')
+      labels.push("");
     }
   }
 
@@ -55,51 +64,55 @@ const Dashboard = (props: Props) => {
     labels: labels,
     datasets: [
       {
-        borderColor: '#ffffff',
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        data: [1221, 2633, 3941, 4992, 6233, 7586],
-      },
-    ],
-  }
+        borderColor: "#ffffff",
+        backgroundColor: "rgba(255, 255, 255, 0.4)",
+        data: [1221, 2633, 3941, 4992, 6233, 7586]
+      }
+    ]
+  };
 
   const geoAPIChartData = {
     labels: labels,
     datasets: [
       {
-        borderColor: '#ffffff',
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        data: [0, 0, 3423, 4865, 5766, 7223],
-      },
-    ],
-  }
+        borderColor: "#ffffff",
+        backgroundColor: "rgba(255, 255, 255, 0.4)",
+        data: [0, 0, 3423, 4865, 5766, 7223]
+      }
+    ]
+  };
 
   const chartOptions = {
     legend: {
-      display: false,
+      display: false
     },
     maintainAspectRatio: false,
     responsive: true,
     scales: {
-      xAxes: [{
-        ticks: {
-          min: 0,
-          fontColor: '#ffffff',
-        },
-        gridLines: {
-          color: 'rgba(255, 255, 255, 0.4)',
-        },
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          fontColor: '#ffffff',
-        },
-        gridLines: {
-          color: 'rgba(255, 255, 255, 0.4)',
-        },
-      }],
+      xAxes: [
+        {
+          ticks: {
+            min: 0,
+            fontColor: "#ffffff"
+          },
+          gridLines: {
+            color: "rgba(255, 255, 255, 0.4)"
+          }
+        }
+      ],
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            fontColor: "#ffffff"
+          },
+          gridLines: {
+            color: "rgba(255, 255, 255, 0.4)"
+          }
+        }
+      ]
     }
-  }
+  };
 
   return (
     <div id="dashboard">
@@ -112,17 +125,28 @@ const Dashboard = (props: Props) => {
         <div className="box-content">
           <h2>Welcome, miya0001!</h2>
           <ul>
-            <li><Link href="#/maps/api-keys" color="inherit" underline="always">Get API key</Link> - Get API key to create your map!</li>
-            <li><Link href="#/data/features" color="inherit" underline="always">Geolonia Locations</Link> - Display your points, lines, polygons on your map application.</li>
+            <li>
+              <Link href="#/maps/api-keys" color="inherit" underline="always">
+                Get API key
+              </Link>{" "}
+              - Get API key to create your map!
+            </li>
+            <li>
+              <Link href="#/data/features" color="inherit" underline="always">
+                Geolonia Locations
+              </Link>{" "}
+              - Display your points, lines, polygons on your map application.
+            </li>
           </ul>
         </div>
       </Paper>
 
       <Grid container className={classes.root} spacing={2}>
-
         <Grid item xs={12} md={6}>
           <Paper className="container-map-loads">
-            <Typography component="h2" className="module-title">Map loads for this month</Typography>
+            <Typography component="h2" className="module-title">
+              Map loads for this month
+            </Typography>
             <div className="chart-container">
               <Line data={mapChartData} options={chartOptions} />
             </div>
@@ -131,7 +155,9 @@ const Dashboard = (props: Props) => {
 
         <Grid item xs={12} md={6}>
           <Paper className="container-geo-api-loads">
-            <Typography component="h2" className="module-title">API loads for this month</Typography>
+            <Typography component="h2" className="module-title">
+              API loads for this month
+            </Typography>
             <div className="chart-container">
               <Line data={geoAPIChartData} options={chartOptions} />
             </div>
@@ -140,24 +166,28 @@ const Dashboard = (props: Props) => {
 
         <Grid item xs={12} md={6}>
           <Paper>
-            <Typography component="h2" className="module-title"><CodeIcon /> API Keys</Typography>
+            <Typography component="h2" className="module-title">
+              <CodeIcon /> API Keys
+            </Typography>
             <Table rows={rowsAPIKeys} permalink="/maps/api-keys/%s" />
           </Paper>
         </Grid>
 
         <Grid item xs={12} md={6}>
           <Paper>
-            <Typography component="h2" className="module-title"><RoomIcon /> Locations</Typography>
+            <Typography component="h2" className="module-title">
+              <RoomIcon /> Locations
+            </Typography>
             <Table rows={rowsFeatures} permalink="/data/gis/%s" />
           </Paper>
         </Grid>
       </Grid>
     </div>
   );
-}
+};
 
 Dashboard.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Dashboard);
