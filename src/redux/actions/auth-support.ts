1592af83eb080f2502d1b2fd12833c9557f925bc
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-09-19 22:37:36 +0900

    Add session ready state to judge whether we can render dashboard

--- src/redux/actions/auth-support.ts
+++ src/redux/actions/auth-support.ts
@@ -2,15 +2,18 @@ import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 
 const SET_CURRENT_USER_ACTION = "AUTH_SUPPORT/SET_CURRENT_USER_ACTION";
 const SET_SESSION_ACTION = "AUTH_SUPPORT/SET_SESSION";
+const READY_ACTION = "AUTH_SUPPORT/READY";
 
 export type AuthSupportState = {
   currentUser?: string;
-  session?: AmazonCognitoIdentity.ICognitoUserSessionData;
+  session?: AmazonCognitoIdentity.CognitoUserSession;
+  isReady: boolean;
 };
 
 const initialState: AuthSupportState = {
   currentUser: void 0,
-  session: void 0
+  session: void 0,
+  isReady: false
 };
 
 type SetCognitoUserAction = {
@@ -20,20 +23,26 @@ type SetCognitoUserAction = {
 
 type SetSessionAction = {
   type: typeof SET_SESSION_ACTION;
-  payload: { session: AmazonCognitoIdentity.ICognitoUserSessionData };
+  payload: { session: AmazonCognitoIdentity.CognitoUserSession };
 };
 
-type AuthSupportAction = SetCognitoUserAction | SetSessionAction;
+type ReadyAction = {
+  type: typeof READY_ACTION;
+  pyload: {};
+};
+
+type AuthSupportAction = SetCognitoUserAction | SetSessionAction | ReadyAction;
 
 export const createActions = {
   setCurrentUser: (currentUser: string) => ({
     type: SET_CURRENT_USER_ACTION,
     payload: { currentUser }
   }),
-  setSession: (session: AmazonCognitoIdentity.ICognitoUserSessionData) => ({
+  setSession: (session: AmazonCognitoIdentity.CognitoUserSession) => ({
     type: SET_SESSION_ACTION,
     payload: { session }
-  })
+  }),
+  ready: () => ({ type: READY_ACTION, payload: {} })
 };
 const isSetCurrentUserAction = (
   action: AuthSupportAction
@@ -43,6 +52,9 @@ const isSetSessionAction = (
   action: AuthSupportAction
 ): action is SetSessionAction => action.type === SET_SESSION_ACTION;
 
+const isReadyAction = (action: AuthSupportAction): action is ReadyAction =>
+  action.type === READY_ACTION;
+
 export const reducer = (
   state: AuthSupportState = initialState,
   action: AuthSupportAction
@@ -51,6 +63,8 @@ export const reducer = (
     return { ...state, currentUser: action.payload.currentUser };
   } else if (isSetSessionAction(action)) {
     return { ...state, session: action.payload.session };
+  } else if (isReadyAction(action)) {
+    return { ...state, isReady: true };
   } else {
     return state;
   }
