09973412e5bbaac577bf9565c3b8376aa3761ab6
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-09-21 22:03:16 +0900

    Some fix

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -1,73 +1,81 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Button from "@material-ui/core/Button";
 
-import Support from './custom/Support';
-import './ResetPassword.scss';
-import Logo from './custom/logo.svg';
-import Alert from './custom/Alert';
-import { resetPassword } from '../auth'
+import Support from "./custom/Support";
+import "./ResetPassword.scss";
+import Logo from "./custom/logo.svg";
+import Alert from "./custom/Alert";
+import { resetPassword } from "../auth";
 
-import { sprintf, __ } from '@wordpress/i18n';
+import { sprintf, __ } from "@wordpress/i18n";
 
-type OwnProps = {}
+type OwnProps = {};
 type RouterProps = {
   history: {
-    push: (path: string) => void
-  }
-}
+    push: (path: string) => void;
+  };
+};
 
-type Props = OwnProps & RouterProps
+type Props = OwnProps & RouterProps;
 
 const Content = (props: Props) => {
-
-  const [password, setPassword] = React.useState('')
-  const [status, setStatus] = React.useState<null | 'success' | 'warning'>(null)
+  const [password, setPassword] = React.useState("");
+  const [status, setStatus] = React.useState<null | "success" | "warning">(
+    null
+  );
   const onPasswordChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setStatus(null)
-    setPassword(e.currentTarget.value)
-  }
+    setStatus(null);
+    setPassword(e.currentTarget.value);
+  };
 
   const handler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setStatus(null)
+    setStatus(null);
     resetPassword(password)
       .then(() => {
-        setStatus('success')
-        setTimeout(() => props.history.push('/'), 2000)
+        setStatus("success");
+        props.history.push("/");
       })
       .catch(err => {
-        setStatus('warning')
-        console.error(err)
-      })
-
-  }
+        setStatus("warning");
+        console.error(err);
+      });
+  };
 
   return (
     <div className="signup">
       <div className="container">
         <img src={Logo} alt="" className="logo" />
-        <h1>{__('Change your password')}</h1>
+        <h1>{__("Change your password")}</h1>
 
         <div className="form">
           <label className="password">
-            <h3>{__('Password')}</h3>
+            <h3>{__("Password")}</h3>
             <input type="text" value={password} onChange={onPasswordChange} />
           </label>
           <label className="confirm-password">
-            <h3>{__('Confirm password')}</h3>
+            <h3>{__("Confirm password")}</h3>
             <input type="text" />
           </label>
-          <p className="message">{__('Make sure it\'s at least 15 characters OR at least 8 characters including a number and a lowercase letter.')}</p>
-          <p><Button variant="contained" color="primary" onClick={handler}>{__('Send password reset email')}</Button></p>
+          <p className="message">
+            {__(
+              "Make sure it's at least 15 characters OR at least 8 characters including a number and a lowercase letter."
+            )}
+          </p>
+          <p>
+            <Button variant="contained" color="primary" onClick={handler}>
+              {__("Send password reset email")}
+            </Button>
+          </p>
         </div>
 
-        <div className="support-container"><Support /></div>
+        <div className="support-container">
+          <Support />
+        </div>
       </div>
     </div>
   );
-}
-
-Content.defaultProps = {
-
 };
+
+Content.defaultProps = {};
 
 export default Content;
