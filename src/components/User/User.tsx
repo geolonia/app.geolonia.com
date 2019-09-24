14f774cccd6f4c80d67542e09d3f29756e075b75
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-09-24 10:15:15 +0900

    Split profile as a component

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -3,15 +3,9 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
-import TextField from "@material-ui/core/TextField";
 import PersonIcon from "@material-ui/icons/Person";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
 import Security from "./security";
-import Save from "../custom/Save";
+import Profile from "./profile";
 import Title from "../custom/Title";
 
 const Content = () => {
@@ -27,11 +21,6 @@ const Content = () => {
 
   const paragraphStyle: React.CSSProperties = {
     marginTop: "1em"
-  };
-
-  const selectStyle: React.CSSProperties = {
-    marginTop: "16px",
-    marginBottom: "8px"
   };
 
   const breadcrumbItems = [
@@ -53,37 +42,7 @@ const Content = () => {
 
       <Grid container spacing={4}>
         <Grid item sm={12} md={8}>
-          <TextField
-            id="standard-name"
-            label="Username"
-            margin="normal"
-            fullWidth={true}
-          />
-
-          <TextField
-            id="standard-name"
-            label="Name"
-            margin="normal"
-            fullWidth={true}
-          />
-
-          <TextField
-            id="standard-name"
-            label="Email"
-            margin="normal"
-            fullWidth={true}
-          />
-
-          <FormControl fullWidth={true} style={selectStyle}>
-            <InputLabel htmlFor="select-language">Language</InputLabel>
-            <Select id="select-language" fullWidth={true}>
-              <MenuItem value="en">English</MenuItem>
-              <MenuItem value="ja">日本語</MenuItem>
-            </Select>
-          </FormControl>
-
-          <Save />
-
+          <Profile />
           <Security />
         </Grid>
 
---
+++ src/components/User/profile.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Link from "@material-ui/core/Link";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Save from "../custom/Save";
+
+type State = {
+  username: string;
+  displayName: string;
+  email: string;
+  language: "en" | "ja";
+};
+
+type Props = {};
+
+const selectStyle: React.CSSProperties = {
+  marginTop: "16px",
+  marginBottom: "8px"
+};
+
+export class Profile extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      username: "",
+      displayName: "",
+      email: "",
+      language: "en"
+    };
+  }
+
+  onUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ username: e.currentTarget.value });
+  };
+
+  onDispayNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ displayName: e.currentTarget.value });
+  };
+
+  onEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ email: e.currentTarget.value });
+  };
+
+  onLanguageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ language: e.currentTarget.value });
+  };
+
+  render() {
+    const { username, displayName, email, language } = this.state;
+
+    return (
+      <>
+        <TextField
+          id="username"
+          label="Username"
+          margin="normal"
+          value={username}
+          onChange={this.onUsernameChange}
+          fullWidth={true}
+        />
+
+        <TextField
+          id="display-name"
+          label="Name"
+          margin="normal"
+          value={displayName}
+          onChange={this.onDispayNameChange}
+          fullWidth={true}
+        />
+
+        <TextField
+          id="email"
+          label="Email"
+          margin="normal"
+          value={email}
+          onChange={this.onEmailChange}
+          fullWidth={true}
+        />
+
+        <FormControl fullWidth={true} style={selectStyle}>
+          <InputLabel htmlFor="select-language">Language</InputLabel>
+          <Select
+            id="select-language"
+            fullWidth={true}
+            value={language}
+            onChange={this.onLanguageChange}
+          >
+            <MenuItem value="en">English</MenuItem>
+            <MenuItem value="ja">日本語</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Save />
+      </>
+    );
+  }
+}
+
+export default Profile;
