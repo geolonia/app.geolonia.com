00dbd86dd4a4ea97902457b909170f23e8ac3fce
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-09-24 17:33:18 +0900

    Add client side file upload

--- src/components/User/avatar.tsx
+++ src/components/User/avatar.tsx
@@ -2,21 +2,45 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import PersonIcon from "@material-ui/icons/Person";
+import Avatar from "@material-ui/core/Avatar";
 
 type Props = {};
 type State = {
   avatarUrl?: string;
+  isFileAPISupported: boolean;
 };
 
 const ProfileImageStyle: React.CSSProperties = {
   width: "250px",
   height: "auto",
-  fill: "#dedede"
+  fill: "#dedede",
+  margin: "auto"
 };
 
-export class Avatar extends React.Component<Props, State> {
-  state = {
-    avatarUrl: void 0
+export class AvatarSection extends React.Component<Props, State> {
+  private inputFileRef: HTMLInputElement | null;
+
+  constructor(props: Props) {
+    super(props);
+    this.inputFileRef = null;
+    this.state = {
+      avatarUrl: void 0,
+      isFileAPISupported: true // TODO: check it
+    };
+  }
+
+  onUploadClick = () => {
+    if (this.inputFileRef) {
+      this.inputFileRef.click();
+    }
+  };
+
+  onFileSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      const avatarUrl = URL.createObjectURL(file);
+      this.setState({ avatarUrl });
+    }
   };
 
   render() {
@@ -24,23 +48,30 @@ export class Avatar extends React.Component<Props, State> {
 
     return (
       <Typography component="p" align="center">
-        <PersonIcon style={ProfileImageStyle} />
+        {avatarUrl ? (
+          <Avatar src={avatarUrl} style={ProfileImageStyle}></Avatar>
+        ) : (
+          <PersonIcon style={ProfileImageStyle} />
+        )}
         <br />
-        <label htmlFor="avatar-file">
-          <Button variant="contained" color="default">
-            Upload new picture
-          </Button>
-        </label>
+        <Button
+          variant="contained"
+          color="default"
+          onClick={this.onUploadClick}
+        >
+          Upload new picture
+        </Button>
         <input
+          ref={ref => (this.inputFileRef = ref)}
           accept="image/*"
           style={{ display: "none" }}
           id="avatar-file"
           type="file"
-          value={avatarUrl}
+          onChange={this.onFileSelected}
         />
       </Typography>
     );
   }
 }
 
-export default Avatar;
+export default AvatarSection;
