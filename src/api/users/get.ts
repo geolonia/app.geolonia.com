cf41e9d915c0b6247fed1d1f4febe95d908bb306
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-09-26 15:49:24 +0900

    Connect to update use meta

--- src/api/users/get.ts
+++ src/api/users/get.ts
@@ -3,13 +3,14 @@ const { REACT_APP_API_BASE } = process.env;
 
 const getUser = (session: AmazonCognitoIdentity.CognitoUserSession) => {
   const userSub = session.getIdToken().decodePayload().sub;
-  const accessToken = session.getIdToken().getJwtToken();
+  const idToken = session.getIdToken().getJwtToken();
 
   return fetch(`${REACT_APP_API_BASE}/users/${userSub}`, {
     method: "GET",
     headers: {
-      Authorization: `bearer ${accessToken}`
+      Authorization: idToken
     }
+    // TODO: handle 40x, 50x
   }).then((res: any) => res.json());
 };
 
