a2a66a78bc437009a71ba0834a3d07c3d9d2cd7b
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-09-29 15:00:50 +0900

    Add group data addition on client side

--- src/redux/actions/group.ts
+++ src/redux/actions/group.ts
@@ -14,20 +14,13 @@ const initialState = { data: [], selectedIndex: 0 };
 
 const SET_ACTION = "GROUPS/SET";
 const SELECT_ACTION = "GROUPS/SELECT";
+const ADD_ACTION = "GROUPS/ADD";
 
-type SetAction = {
-  type: typeof SET_ACTION;
-  payload: {
-    groups: Group[];
-  };
-};
+type SetAction = { type: typeof SET_ACTION; payload: { groups: Group[] } };
+type SelectAction = { type: typeof SELECT_ACTION; payload: { index: number } };
+type AddAction = { type: typeof ADD_ACTION; payload: { group: Group } };
 
-type SelectAction = {
-  type: typeof SELECT_ACTION;
-  payload: { index: number };
-};
-
-type GroupAction = SetAction | SelectAction;
+type GroupAction = SetAction | SelectAction | AddAction;
 
 const isSetAction = (action: GroupAction): action is SetAction =>
   action.type === SET_ACTION;
@@ -35,9 +28,13 @@ const isSetAction = (action: GroupAction): action is SetAction =>
 const isSelectAction = (action: GroupAction): action is SelectAction =>
   action.type === SELECT_ACTION;
 
+const isAddAction = (action: GroupAction): action is AddAction =>
+  action.type === ADD_ACTION;
+
 export const createActions = {
   set: (groups: Group[]) => ({ type: SET_ACTION, payload: { groups } }),
-  select: (index: number) => ({ type: SELECT_ACTION, payload: { index } })
+  select: (index: number) => ({ type: SELECT_ACTION, payload: { index } }),
+  add: (group: Group) => ({ type: ADD_ACTION, payload: { group } })
 };
 
 export const reducer = (
@@ -48,6 +45,8 @@ export const reducer = (
     return { ...state, data: action.payload.groups };
   } else if (isSelectAction(action)) {
     return { ...state, selectedIndex: action.payload.index };
+  } else if (isAddAction(action)) {
+    return { ...state, data: [...state.data, action.payload.group] };
   } else {
     return state;
   }
