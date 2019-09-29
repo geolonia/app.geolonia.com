72bdbf7f25fe9f6c903bb44b01279e64b63bf284
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-09-29 22:43:22 +0900

    i18n

--- src/lib/loadLocale.ts
+++ src/lib/loadLocale.ts
@@ -1,5 +1,10 @@
 import ja from "../lang/ja.json"; // TODO: This is a mock up
 
-export function loadLocale() {
-  return ja.locale_data.messages;
+export function loadLocale(lang: string) {
+  console.log(lang)
+  if ('ja' === lang) {
+    return ja.locale_data.messages
+  } else {
+    return {}
+  }
 }
