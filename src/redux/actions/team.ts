0b75747aead317a144aef20ddd8dcadb6c0e6988
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-10-05 09:50:22 +0900

    Enable team props update on Team-General page

--- src/redux/actions/team.ts
+++ src/redux/actions/team.ts
@@ -15,15 +15,19 @@ export type TeamState = {
 
 const initialState = { data: [], selectedIndex: 0 };
 
-const SET_ACTION = "TEAMS/SET";
-const SELECT_ACTION = "TEAMS/SELECT";
-const ADD_ACTION = "TEAMS/ADD";
+const SET_ACTION = "TEAM/SET";
+const SELECT_ACTION = "TEAM/SELECT";
+const ADD_ACTION = "TEAM/ADD";
+const UPDATE_ACTION = "TEAM/UPDATE";
 
 type SetAction = { type: typeof SET_ACTION; payload: { teams: Team[] } };
 type SelectAction = { type: typeof SELECT_ACTION; payload: { index: number } };
 type AddAction = { type: typeof ADD_ACTION; payload: { team: Team } };
-
-type TeamAction = SetAction | SelectAction | AddAction;
+type UpdateAction = {
+  type: typeof UPDATE_ACTION;
+  payload: { index: number; team: Partial<Team> };
+};
+type TeamAction = SetAction | SelectAction | AddAction | UpdateAction;
 
 const isSetAction = (action: TeamAction): action is SetAction =>
   action.type === SET_ACTION;
@@ -34,10 +38,17 @@ const isSelectAction = (action: TeamAction): action is SelectAction =>
 const isAddAction = (action: TeamAction): action is AddAction =>
   action.type === ADD_ACTION;
 
+const isUpdateAciton = (action: TeamAction): action is UpdateAction =>
+  action.type === UPDATE_ACTION;
+
 export const createActions = {
   set: (teams: Team[]) => ({ type: SET_ACTION, payload: { teams } }),
   select: (index: number) => ({ type: SELECT_ACTION, payload: { index } }),
-  add: (team: Team) => ({ type: ADD_ACTION, payload: { team } })
+  add: (team: Team) => ({ type: ADD_ACTION, payload: { team } }),
+  update: (index: number, team: Partial<Team>) => ({
+    type: UPDATE_ACTION,
+    payload: { index, team }
+  })
 };
 
 export const reducer = (
@@ -50,7 +61,15 @@ export const reducer = (
     return { ...state, selectedIndex: action.payload.index };
   } else if (isAddAction(action)) {
     return { ...state, data: [...state.data, action.payload.team] };
-  } else {
+  } else if (isUpdateAciton(action)) {
+    const nextTeams = [...state.data];
+    nextTeams[action.payload.index] = {
+      ...nextTeams[action.payload.index],
+      ...action.payload.team
+    };
+    return { ...state, data: nextTeams };
+  }
+  {
     return state;
   }
 };
