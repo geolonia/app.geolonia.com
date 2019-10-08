7e85bdde013bbc657dc5e47ad4b9ffd16cc0703d
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-10-08 11:07:35 +0900

    Add reduxify method to connect redux

---
+++ src/redux/reduxify.ts
@@ -0,0 +1,61 @@
+import { connect } from "react-redux";
+import { AppState } from "./store";
+import Redux from "redux";
+import { reducer } from "./actions/user-meta";
+
+type AppStateCompositionKey = keyof AppState;
+type ReduxifySetStateParam = { [key in AppStateCompositionKey]: any };
+
+export const GLOBAL_UTIL_ACTION_TYPE = "UTIL/REDIXFY";
+export type GLOBAL_UTIL_ACITON = {
+  type: typeof GLOBAL_UTIL_ACTION_TYPE;
+  payload: { key: string; nextLocalState: any };
+};
+
+export const appendReduxifyReducers: Redux.Reducer = (
+  reducerMapObjects: Redux.ReducersMapObject
+) => {
+  const result: any = {};
+  for (let key in reducerMapObjects) {
+    result[key] = (state: any, action: any) => {
+      const nextState = reducerMapObjects[key](state, action);
+
+      if (
+        action.type === GLOBAL_UTIL_ACTION_TYPE &&
+        key === action.payload.key &&
+        action.payload.nextLocalState
+      ) {
+        return action.payload.nextLocalState;
+      } else {
+        return nextState;
+      }
+    };
+  }
+  return result;
+};
+
+const reduxify = (MyComponent: any) => {
+  const mapStateToProps = (state: AppState) => {
+    return { state };
+  };
+  const mapDispatchToProps = (dispatch: Redux.Dispatch) => {
+    return {
+      dispatch,
+      setState: (param: ReduxifySetStateParam) => {
+        (Object.keys(param) as AppStateCompositionKey[]).forEach(key => {
+          param[key] &&
+            dispatch({
+              type: GLOBAL_UTIL_ACTION_TYPE,
+              payload: { key, nextLocalState: param[key] }
+            });
+        });
+      }
+    };
+  };
+  return connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(MyComponent);
+};
+
+export default reduxify;
