f81b79fb74030d6bde98952331c84fcf10583f09
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-10-08 11:07:35 +0900

    Add reduxify method to connect redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import {
   reducer as authSupportReducer,
   AuthSupportState
@@ -13,6 +13,9 @@ import {
   reducer as teamMemberReducer,
   State as TeamMemberState
 } from "./actions/team-member";
+import Redux from "redux";
+
+import { appendReduxifyReducers } from "./reduxify";
 
 // app type
 export type AppState = {
@@ -23,13 +26,22 @@ export type AppState = {
   teamMember: TeamMemberState;
 };
 
-const appReducer = combineReducers({
-  authSupport: authSupportReducer,
-  userMeta: userMetaReducer,
-  team: teamReducer,
-  mapKey: mapKeyReducer,
-  teamMember: teamMemberReducer
-});
+const appReducer = combineReducers(
+  // @ts-ignore
+  appendReduxifyReducers({
+    authSupport: authSupportReducer,
+    userMeta: userMetaReducer,
+    team: teamReducer,
+    mapKey: mapKeyReducer,
+    teamMember: teamMemberReducer
+  })
+);
+
+// middleware
+export const middleware: Redux.Middleware = store => next => action => {
+  // console.log(action);
+  return next(action);
+};
 
 // store
-export default createStore(appReducer);
+export default createStore(appReducer, applyMiddleware(middleware));
