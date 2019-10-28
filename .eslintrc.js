0582b30139368273ad2743b6f660cf96efc716e0
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-10-28 22:09:24 +0900

    Add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,22 @@
-module.exports = {}
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  plugins: ["react", "@typescript-eslint"],
+  env: {
+    browser: true,
+    jest: true
+  },
+  rules: {
+    "no-console": "error"
+  },
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect"
+    }
+  },
+  parser: "@typescript-eslint/parser"
+};
