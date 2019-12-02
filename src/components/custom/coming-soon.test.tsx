8b7d8e874fe3b09dc0d82c56208f1cc00ce6b6a3
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-12-02 13:48:16 +0900

    Fix test

--- src/components/custom/coming-soon.test.tsx
+++ src/components/custom/coming-soon.test.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { ComingSoon } from "./coming-soon";
 import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
 
-test("it should render label", () => {
+test("it should render without crash", () => {
   const component = renderer.create(
     <ComingSoon>
       <div style={{ width: 100, height: 100 }}>
@@ -12,4 +13,18 @@ test("it should render label", () => {
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test("it should render the label and inner content", async () => {
+  const result = render(
+    <ComingSoon label={"coming soon!"}>
+      <div style={{ width: 100, height: 100 }}>
+        <span>{"hello"}</span>
+      </div>
+    </ComingSoon>
+  );
+  const labels = await result.findAllByText("coming soon!");
+  const contents = await result.findAllByText("hello");
+  expect(labels).toHaveLength(1);
+  expect(contents).toHaveLength(1);
 });
