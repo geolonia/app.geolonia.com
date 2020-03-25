4b865152874e8384958f5b53879e938e00c8b647
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-03-26 05:39:02 +0900

    some fix

--- src/components/custom/InputColor.tsx
+++ src/components/custom/InputColor.tsx
@@ -15,6 +15,10 @@ interface ColorObject {
 }
 
 const blackOrWhite = (hexcolor: string) => {
+  if (! hexcolor.startsWith('#')) {
+    return '#FFFFFF'
+  }
+
 	var r = parseInt(hexcolor.substr( 1, 2 ), 16)
 	var g = parseInt(hexcolor.substr( 3, 2 ), 16)
 	var b = parseInt(hexcolor.substr( 5, 2 ), 16)
