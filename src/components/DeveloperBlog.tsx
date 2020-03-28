ffa7701f6dbc4f1738b7e544169315ea9624ad6b
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-03-28 23:15:22 +0900

    some fix

--- src/components/DeveloperBlog.tsx
+++ src/components/DeveloperBlog.tsx
@@ -4,12 +4,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { __ } from "@wordpress/i18n";
 
 type BlogPost = {
   id : string;
