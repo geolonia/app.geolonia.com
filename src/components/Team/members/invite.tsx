83fcdffad297f4139e70911f67c9c6b418f12da9
kamataryo <kamataryo@users.noreply.github.com>
2020-05-26 14:04:31 +0900

    Fix invitation sending UI at /team/members

--- src/components/Team/members/invite.tsx
+++ src/components/Team/members/invite.tsx
@@ -5,16 +5,12 @@ import AddNew from "../../custom/AddNew";
 
 // Util
 import { __ } from "@wordpress/i18n";
-
-// API
-import addMember from "../../../api/members/add";
+import fetch from "../../../lib/fetch";
 
 // Types
 import { AppState, Session, Team, Member } from "../../../types";
 
 // redux
-import Redux from "redux";
-import { createActions as createTeamMemberActions } from "../../../redux/actions/team-member";
 import { connect } from "react-redux";
 
 type OwnProps = {
@@ -26,38 +22,40 @@ type StateProps = {
   members: Member[];
   team: Team | void;
 };
-type DispatchProps = {
-  addMemberState: (teamId: string, member: Member) => void;
-};
-type Props = OwnProps & StateProps & DispatchProps;
+type Props = OwnProps & StateProps;
 
 export const Invite = (props: Props) => {
   const [message, setMessage] = React.useState("");
 
   const inviteHandler = (email: string) => {
-    const { session, team, addMemberState, members } = props;
+    const { session, team, members } = props;
 
     if (members.find(member => member.email === email)) {
-      setMessage(__("They are already a member of the team."));
+      setMessage(__("They is already a member of this team."));
       return Promise.reject("They are already a member of the team.");
     }
 
     if (team) {
-      return addMember(session, team.teamId, email).then(result => {
-        if (result.error) {
-          setMessage(result.message);
-          throw new Error(result.code);
+      return fetch(
+        session,
+        `https://api.app.geolonia.com/${process.env.REACT_APP_STAGE}/teams/${team.teamId}/invitation`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ email })
+        }
+      ).then(res => {
+        if (res.status < 400) {
+          return res.json();
         } else {
-          const newMember = result.data;
-          if (members.find(member => member.userSub === newMember.userSub)) {
-            console.warn("Already joined");
-          } else {
-            addMemberState(team.teamId, newMember);
-          }
+          setMessage(__("Please set correct email address."));
+          throw new Error();
         }
       });
     } else {
-      return Promise.reject("Unknown Error");
+      return Promise.reject("No team");
     }
   };
 
@@ -65,9 +63,9 @@ export const Invite = (props: Props) => {
     <AddNew
       disabled={props.disabled}
       buttonLabel={__("Invite")}
-      label={__("Invite a member")}
+      label={__("Send an invitation")}
       description={__(
-        "We automatically update your billing as you add and remove team members."
+        "We automatically update your billing as your invitation is accepted."
       )}
       defaultValue=""
       fieldName="email"
@@ -94,9 +92,4 @@ export const mapStateToProps = (state: AppState): StateProps => {
   return { session, team, members };
 };
 
-export const mapDispatchToProps = (dispatch: Redux.Dispatch) => ({
-  addMemberState: (teamId: string, member: Member) =>
-    dispatch(createTeamMemberActions.add(teamId, member))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Invite);
+export default connect(mapStateToProps)(Invite);
