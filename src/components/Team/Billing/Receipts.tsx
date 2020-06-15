9918d634e136426dee44617803e6cbf32212dbe8
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2020-06-15 14:50:58 +0900

    some fix

---
+++ src/components/Team/Billing/Receipts.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+
+};
+
+const Content = (props: Props) => {
+  return (
+    <div>
+      ここに支払履歴のテーブル
+    </div>
+  );
+};
+
+export default Content;
