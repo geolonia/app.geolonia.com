3e5efe948c688af0cc7c1d772696c3359fa82609
kamataryo <kamataryo@users.noreply.github.com>
2020-08-15 10:36:55 +0900

    Type enhancement

--- src/lib/read-file.ts
+++ src/lib/read-file.ts
@@ -1,7 +1,7 @@
 export const readFile = (file: File) => {
   return new Promise<string>((resolve, reject) => {
     const reader = new FileReader();
-    reader.onload = (e: any) => {
+    reader.onload = (e: ProgressEvent<FileReader>) => {
       resolve(e.target && e.target.result ? (e.target.result as string) : "");
     };
     reader.onerror = () => reject(reader.error);
