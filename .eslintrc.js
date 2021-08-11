4c772a4c65d797dfba8acf1ca0e87e3f70d8b933
kamataryo <kamataryo@users.noreply.github.com>
2021-08-11 18:57:55 +0900

    Use Geolonia shared ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,22 @@
 module.exports = {
-  extends: [
-    "react-app",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  plugins: ["react", "@typescript-eslint"],
+  extends: ['@geolonia', 'react-app', 'plugin:react/recommended'],
+  plugins: ['react'],
   env: {
     browser: true,
-    jest: true
-  },
-  rules: {
-    "no-console": "warn",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "react/prop-types": "off", // TypeScript will take care of prop validation
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    jest: true,
   },
   settings: {
     react: {
-      pragma: "React",
-      version: "detect"
-    }
+      pragma: 'React',
+      version: 'detect',
+    },
   },
   overrides: [
     {
-      files: ["**/*.tsx"],
+      files: ['**/*.tsx'],
       rules: {
-        "react/prop-types": "off"
-      }
-    }
+        'react/prop-types': 'off',
+      },
+    },
   ],
-  parser: "@typescript-eslint/parser"
 };
