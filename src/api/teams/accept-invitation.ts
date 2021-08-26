9f0b91b25996ca703ecdd881d81e8e97a4afc5c5
kamataryo <kamataryo@users.noreply.github.com>
2021-08-26 18:36:54 +0900

    Fix endpoint accept invitation

--- src/api/teams/accept-invitation.ts
+++ src/api/teams/accept-invitation.ts
@@ -2,7 +2,6 @@ import { buildApiAppUrl } from '../../lib/api';
 import customFetch from '../../lib/fetch';
 
 export const acceptInvitation = (invitationToken: string, session?: Geolonia.Session) => {
-  const url = buildApiAppUrl('/accept-invitation');
   const fetchOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'Application/json' },
@@ -10,8 +9,10 @@ export const acceptInvitation = (invitationToken: string, session?: Geolonia.Ses
   };
 
   if(session) {
+    const url = buildApiAppUrl('/accept-team-invitation');
     return customFetch(session, url, fetchOptions);
   } else {
+    const url = buildApiAppUrl('/accept-invitation');
     return fetch(url, fetchOptions);
   }
 };
