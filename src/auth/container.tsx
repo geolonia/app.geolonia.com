81f7d69b724027179f1dc66b8757727e366e1474
Keitaroh Kobayashi <keita@kbys.me>
2021-09-17 11:02:18 +0900

    UI improvements (#595)
    * Show user name in header
    
    * Update authcontainer to functional component
    
    * Fix user login failure message
    
    * Add Logged in as.. to profile menu

--- src/auth/container.tsx
+++ src/auth/container.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // i18n
 import { loadLocale } from '../lib/load-locale';
@@ -12,7 +12,7 @@ import getUser from '../api/users/get';
 import estimateLanguage from '../lib/estimate-language';
 
 // redux
-import { connect } from 'react-redux';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import {
   getInTrouble,
   ready,
@@ -20,140 +20,72 @@ import {
 } from '../redux/actions/auth-support';
 import {
   set as setUserMeta,
-  setAvatar,
   isUserMeta,
 } from '../redux/actions/user-meta';
 
 // Types
-import Redux from 'redux';
 import Moment from 'moment';
+
 import mixpanel from 'mixpanel-browser';
 
-type OwnProps = { children: React.ReactElement };
-type StateProps = { session: Geolonia.Session };
-type DispatchProps = {
-  serverTrouble: () => void;
-  ready: () => void;
-  setLoggedIn: (loggedIn: boolean) => void;
-  setUserMeta: (userMeta: Geolonia.User) => void;
-  setUserAvatar: (avatarImage: string | void) => void;
-};
-type Props = OwnProps & StateProps & DispatchProps;
+const AuthContainer: React.FC = ({children}) => {
+  const dispatch = useAppDispatch();
+  const { isReady } = useAppSelector((state) => ({
+    isReady: state.authSupport.isReady,
+  }));
 
-type State = Record<string, never>;
+  useEffect(() => {
+    (async () => {
+      const session = await getSession();
 
-type FundamentalAPIResult = {
-  teams: Geolonia.Team[];
-  user: Geolonia.User;
-};
-
-const fundamentalAPILoads = (session: Geolonia.Session) => {
-  if (!session) {
-    return Promise.reject('nosession');
-  }
-  return Promise.all([
-    getUser(session),
-    /*more API loads here*/
-  ]).then(([userResult]) => {
-    const user = userResult.error
-      ? void 0
-      : { ...userResult.data.item, links: userResult.data.links };
-    return {
-      user,
-    };
-  });
-};
-
-export class AuthContainer extends React.Component<Props, State> {
-  async componentDidMount() {
-    const session = await getSession();
-
-    if (session === null) {
-      setLocaleData(loadLocale(estimateLanguage()));
-      this.props.setLoggedIn(false);
-      return this.props.ready();
-    }
-
-    const idTokenPayload = session.getIdToken().payload;
-    mixpanel.identify(idTokenPayload.sub);
-    mixpanel.alias(idTokenPayload['cognito:username']);
-    mixpanel.people.set('$name', idTokenPayload['cognito:username']);
-    mixpanel.people.set('$email', idTokenPayload.email);
-
-    try {
-      const { user } = (await fundamentalAPILoads(
-        session,
-      )) as FundamentalAPIResult;
-      if (!isUserMeta(user)) {
-        throw new Error('invalid user meta response');
+      if (session === null) {
+        setLocaleData(loadLocale(estimateLanguage()));
+        dispatch(setLoggedIn(false));
+        dispatch(ready());
+        return;
       }
 
-      // ログイン時に毎回ユーザーの言語をローカルストレージに保存しておく。
-      // ログアウトしたときの多言語化で使うため
-      if (user.language) {
-        localStorage.setItem('geolonia__persisted_language', user.language);
+      const idTokenPayload = session.getIdToken().payload;
+      mixpanel.identify(idTokenPayload.sub);
+      mixpanel.alias(idTokenPayload['cognito:username']);
+      mixpanel.people.set('$name', idTokenPayload['cognito:username']);
+      mixpanel.people.set('$email', idTokenPayload.email);
+
+      try {
+        const userResp = await getUser(session);
+        const user = userResp.error ? undefined : { ...userResp.data.item, links: userResp.data.links };
+        if (!isUserMeta(user)) {
+          throw new Error('invalid user meta response');
+        }
+
+        const { language, timezone } = user;
+        // ログイン時に毎回ユーザーの言語をローカルストレージに保存しておく。
+        // ログアウトしたときの多言語化で使うため
+        if (language) {
+          localStorage.setItem('geolonia__persisted_language', language);
+        }
+
+        const localeData = loadLocale(language);
+        if (localeData) {
+          setLocaleData(localeData);
+        }
+        Moment.locale(language);
+        Moment.tz.setDefault(timezone);
+        // NOTE: We can localize datetime format here
+        Moment.defaultFormat = 'YYYY-MM-DD HH:mm:ss';
+
+        dispatch(setLoggedIn(true));
+        dispatch(setUserMeta(user));
+      } catch (error) {
+        dispatch(getInTrouble());
+        dispatch(setLoggedIn(false));
+      } finally {
+        dispatch(ready());
       }
+    })();
+  }, [dispatch]);
 
-      this.props.setLoggedIn(true);
-      this.props.setUserMeta(user);
+  return <>{isReady && children}</>;
+};
 
-      const { language, timezone } = user;
-      const localeData = loadLocale(language);
-      if (localeData) {
-        setLocaleData(localeData);
-      }
-      Moment.locale(language);
-      Moment.tz.setDefault(timezone);
-      // NOTE: We can localize datetime format here
-      Moment.defaultFormat = 'YYYY-MM-DD HH:mm:ss';
-
-    } catch (error) {
-      this.props.serverTrouble();
-      this.props.setLoggedIn(false);
-    } finally {
-      this.props.ready();
-    }
-  }
-
-  _handleAvatarResponse = (res: Response) => {
-    if (res.ok) {
-      return res.blob().then(URL.createObjectURL);
-    } else {
-      throw new Error('Request failed');
-    }
-  };
-
-  loadAvatars = (userMeta: Geolonia.User) => {
-    return Promise.all([
-      userMeta.links.getAvatar
-        ? fetch(userMeta.links.getAvatar)
-          .then(this._handleAvatarResponse)
-          .catch((err) => {
-            return void 0;
-          })
-        : void 0,
-    ]).then(([userAvatarImage]) => {
-      this.props.setUserAvatar(userAvatarImage);
-    });
-  };
-
-  render() {
-    const { children } = this.props;
-    return <>{children}</>;
-  }
-}
-
-const mapStateToProps = (state: Geolonia.Redux.AppState): StateProps => ({
-  session: state.authSupport.session,
-});
-
-const mapDispatchToProps = (dispatch: Redux.Dispatch): DispatchProps => ({
-  serverTrouble: () => dispatch(getInTrouble()),
-  ready: () => dispatch(ready()),
-  setLoggedIn: (loggedIn) => dispatch(setLoggedIn(loggedIn)),
-  setUserMeta: (userMeta) => dispatch(setUserMeta(userMeta)),
-  setUserAvatar: (avatarImage) =>
-    dispatch(setAvatar({avatarImage})),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(AuthContainer);
+export default AuthContainer;
